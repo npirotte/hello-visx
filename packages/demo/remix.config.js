62b543a5ea549b5274460cc06d119123eb522143
Nicolas Pirotte <nicolas.pirotte@aaqua.live>
2022-04-11 14:05:50 +0200

    try to fix it again

--- packages/demo/remix.config.js
+++ packages/demo/remix.config.js
@@ -3,8 +3,8 @@
  */
 module.exports = {
   appDirectory: "app",
-  browserBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "api/_build",
+  browserBuildDirectory: "../../public/build",
+  publicPath: "../../build/",
+  serverBuildDirectory: "../../api/_build",
   devServerPort: 8002,
 };
---
+++ public/build/routes/demos/correct-RKSGT2RN.js
@@ -0,0 +1,1 @@
+import{f as o,g as t}from"../../build/_shared/chunk-ICDE4O2B.js";t();t();function r(){return o.createElement("h1",null,"You got it right!")}export{r as default};
