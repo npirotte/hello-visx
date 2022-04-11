91f446c27b6ce4a0c4b06da459984b75a4f5cf2d
Nicolas Pirotte <nicolas.pirotte@aaqua.live>
2022-04-11 14:09:23 +0200

    fix vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const { createRequestHandler } = require('@remix-run/vercel');
 
 module.exports = createRequestHandler({
-  build: require('../packages/demo/api/_build'),
+  build: require('./_build'),
 });
